3f6c201a01b8b8bd48570b2d592865a49cb3b5a2
OpenSauce <lawleycobhc@gmail.com>
2024-12-06 15:56:36 +0000

    Test

--- src/config/settings.py
+++ src/config/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "/static/"
 STATICFILES_DIRS = ["/public", os.path.join(BASE_DIR, "..", "public")]
-STATIC_ROOT = "/app/public_collected"
+STATIC_ROOT = "/public_collected"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Django Debug Toolbar
