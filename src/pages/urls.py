3f86316cbf930851a888c38cbd48ade155e46988
OpenSauce <lawleycobhc@gmail.com>
2024-12-15 20:04:21 +0000

    Add robots.txt and sitemap.xml

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
 
 from pages import views
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("vocabulary/", views.vocabulary, name="vocabulary"),
     path("resources/", views.resources, name="resources"),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
